bdb1db9619f1d14b33410217889b4a17d431ca04
YY酱 <yy@YYjiangdeMacBook-Pro.local>
2023-05-31 23:53:48 +0800

    feat: add custom colors && fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,22 @@ module.exports = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        polySans: ['url("/fonts/PolySans-Median.otf") format("otf")'],
+      },
+      colors: {
+        va: {
+          blue: '#3232FB',
+          black: '#1A1A1A',
+          coral: '#FF6E40',
+          forest: '#005B25',
+          lime: '#DCFF78',
+          lavender: '#EDE9FF',
+        },
       },
     },
   },
   plugins: [],
-}
+};
