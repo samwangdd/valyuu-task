279e563eb0ee764245ffe10425b9b37ddbcfdca4
sam <wyysammore@gmail.com>
2023-06-06 15:28:08 +0800

    fix(save-planet): UI suitable tablet

--- app/sell/components/save-plant-component.tsx
+++ app/sell/components/save-plant-component.tsx
@@ -21,14 +21,14 @@ export default function SavePlantComponent(props: ISavePlantComponentProps) {
           <p className='p1 mb-2 '>We believe</p>
           <p className='h3 mb-1'>Save planet is something everyone can do</p>
         </div>
-        <div className='md:flex xl:h-[29.5rem] xl:flex-1 xl:relative'>
+        <div className='sm:flex xl:h-[29.5rem] xl:flex-1 xl:relative'>
           <Card
             isHovered={!isHovered}
             src='/e62ba45a-df70-4ef5-932e-0a0a023d053e.png'
             title='Sustainability'
             description='donate the value of your product to Stichting Leergeld.'
             onClick={() => {}}
-            className={`bg-va-lavender mt-5 md:flex-1 xl:absolute xl:transform transition-all xl:duration-500 xl:rotate-[-7deg] xl:translate-y-5 ${
+            className={`bg-va-lavender mt-5 sm:flex-1 xl:absolute xl:transform transition-all xl:duration-500 xl:rotate-[-7deg] xl:translate-y-5 ${
               isHovered ? 'xl:w-[333px] xl:rotate-[7deg]' : ''
             }`}
           />
@@ -40,7 +40,7 @@ export default function SavePlantComponent(props: ISavePlantComponentProps) {
             onClick={() => {}}
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
-            className={`bg-va-lime mt-5 transition-all md:flex-1 xl:absolute xl:transform xl:duration-500  xl:translate-y-4 ${
+            className={`bg-va-lime mt-5 transition-all sm:flex-1 xl:absolute xl:transform xl:duration-500  xl:translate-y-4 ${
               isHovered
                 ? 'xl:rotate-[-8.5deg] xl:translate-x-[330px]'
                 : 'xl:rotate-[8.5deg] xl:translate-x-[620px] xl:w-[333px]'
